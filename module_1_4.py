4b6a19ebdcb37c0ab89507f5290fb71b5198f835
isa <alfred.nikitin@Mail.ru>
2024-08-18 20:44:51 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+a = 'Ответы: '
+def out_red(text):
+    print("\033[36m{}".format(text))
+out_red("Организация программ и методы строк:")
+my_string = input('Напишите любой текст:')
+print(my_string)
+
+print(my_string . upper())
+print(my_string . lower())
+print(my_string .replace(' ',''))
+print(my_string [-1])
